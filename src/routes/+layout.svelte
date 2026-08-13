91597ab95f5d14620b7d1b031881d701c78bc05e
lukas <67807954+lukasabbe@users.noreply.github.com>
2026-08-13 22:55:19 +0200

    Init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.png';
+
+	let { children } = $props();
+</script>
+
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<title>Lukas Website</title>
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    import favicon from '$lib/assets/favicon.png';
+    let downloads = $state(0);
+    async function main(){
+        downloads = await fetchDownloads();
+    }
+    async function fetchDownloads() {
+        const response = await fetch('https://api.modrinth.com/v2/user/QCe37V9V/projects');
+        const data = await response.json();
+        let totalDownloads = 0;
+        for (const project of data) {
+            totalDownloads += project.downloads;
+        }
+        return totalDownloads;
+    }
+    main();
+</script>
+
+<main class="min-h-screen flex flex-col items-center">
+    <img src={favicon} alt="icon" class="w-80 h-80 rounded-full mb-8 m-10">
+    <div class="flex flex-col items-center">
+        <h1 class="text-4xl font-bold text-white">Lukas</h1>
+        <p class="text-white">Programmer and Software Engineer student</p>
+    </div>
+    <div class="flex flex-col items-center mt-8">
+        <h1 class="text-2xl font-bold text-white">Links:</h1>
+        <div class="flex flex-col items-center mt-4">
+            <a href="https://github.com/lukasabbe/" class="text-blue-400 hover:underline">GitHub</a>
+            <a href="https://modrinth.com/user/lukasabbe" class="text-blue-400 hover:underline">Modrinth</a>
+        </div>
+    </div>
+    <div class="flex flex-col items-center mt-8">
+        <h1 class="text-2xl font-bold text-white">Stats:</h1>
+        <div class="flex flex-col items-center mt-4">
+            <p class="text-white">Total Modrinth Downloads: {downloads}</p>
+        </div>
+    </div>
+</main>
