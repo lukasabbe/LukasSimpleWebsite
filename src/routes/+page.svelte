4352e95bdd040e294136b9a08a2476532acd8131
lukas <67807954+lukasabbe@users.noreply.github.com>
2026-08-19 13:42:57 +0200

    Added cool background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,20 +19,20 @@
 <main class="min-h-screen flex flex-col items-center">
     <img src={favicon} alt="icon" class="w-80 h-80 rounded-full mb-8 m-10">
     <div class="flex flex-col items-center">
-        <h1 class="text-4xl font-bold text-white">Lukas</h1>
-        <p class="text-white">Programmer and Software Engineer student</p>
+        <h1 class="text-7xl font-bold text-white">Lukas</h1>
+        <p class="text-3xl text-white font-bold">Programmer and Software Engineer student</p>
     </div>
     <div class="flex flex-col items-center mt-8">
-        <h1 class="text-2xl font-bold text-white">Links:</h1>
+        <h1 class="text-3xl font-bold text-white">Links:</h1>
         <div class="flex flex-col items-center mt-4">
-            <a href="https://github.com/lukasabbe/" class="text-blue-400 hover:underline">GitHub</a>
-            <a href="https://modrinth.com/user/lukasabbe" class="text-blue-400 hover:underline">Modrinth</a>
+            <a href="https://github.com/lukasabbe/" class="text-white font-bold hover:underline">GitHub</a>
+            <a href="https://modrinth.com/user/lukasabbe" class="text-white font-bold hover:underline">Modrinth</a>
         </div>
     </div>
     <div class="flex flex-col items-center mt-8">
-        <h1 class="text-2xl font-bold text-white">Stats:</h1>
+        <h1 class="text-3xl font-bold text-white">Stats:</h1>
         <div class="flex flex-col items-center mt-4">
-            <p class="text-white">Total Modrinth Downloads: {downloads}</p>
+            <p class="text-white font-bold">Total Modrinth Downloads: {downloads}</p>
         </div>
     </div>
 </main>
