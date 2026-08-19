95aa9b97a4e697cfb9980de8bf2a9a7cccc6dfb2
lukas <67807954+lukasabbe@users.noreply.github.com>
2026-08-19 13:19:41 +0200

    Fix?

--- svelte.config.js
+++ svelte.config.js
@@ -3,16 +3,25 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
-  kit: {
-    adapter: adapter({
-      pages: 'dist',
-      assets: 'dist',
-      fallback: 'index.html',
-      precompress: false,
-      strict: true
-    })
-  }
+    preprocess: vitePreprocess(),
+
+    vitePlugin: {
+        dynamicCompileOptions({ filename }) {
+            if (!filename.split(/[/\\]/).includes('node_modules')) {
+                return { runes: true };
+            }
+        }
+    },
+
+    kit: {
+        adapter: adapter({
+            pages: 'dist',
+            assets: 'dist',
+            fallback: 'index.html',
+            precompress: false,
+            strict: true
+        })
+    }
 };
 
 export default config;
